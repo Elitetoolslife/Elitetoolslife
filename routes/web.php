dbb055c1bd5d9f947d7776bc2ef3a6be649253b6
Elitetoolslife <elitetoolsife@gmail.com>
2024-12-07 13:08:22 +1345

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,10 @@
+<?php
 
+use App\Http\Controllers\BalanceController;
+
+
+
+
+
+Route::get('/add-balance', [BalanceController::class, 'showForm'])->name('balance.form');
+Route::post('/add-balance', [BalanceController::class, 'addBalance'])->name('balance.add');
