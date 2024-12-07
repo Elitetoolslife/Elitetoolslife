1665fde9e2fa421be2dcec5cbf3dee39bad751d8
EliteTools <elitetoolsife@gmail.com>
2024-12-07 10:17:43 +0100

    Update ReplyController.php

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -9,36 +9,55 @@ use Carbon\Carbon;
 
 class ReplyController extends Controller
 {
+    /**
+     * Handle the reply to a ticket.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function addReply(Request $request, $id)
     {
-        $uid = Auth::user()->id;
-        $rep = $request->input('Reply');
+        $userId = Auth::user()->id;
+        $reply = $request->input('Reply');
         
-        if (empty($rep)) {
+        if (empty($reply)) {
             return response("01", 400);
         }
 
-        $ticket = Ticket::where('id', $id)->where('uid', $uid)->first();
+        $ticket = Ticket::where('id', $id)->where('user_id', $userId)->first();
 
         if ($ticket && $ticket->status == 1) {
-            $msg = '
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <div class="ticket">' . htmlspecialchars($rep) . '</div>
-                    </div>
-                    <div class="panel-footer">
-                        <div class="label label-info">' . $uid . '</div> - ' . Carbon::now()->format('d/m/Y h:i:s a') . '
-                    </div>
-                </div>';
+            $message = $this->formatMessage($reply, $userId);
 
             $ticket->update([
-                'memo' => $ticket->memo . $msg,
+                'memo' => $ticket->memo . $message,
                 'seen' => 0,
-                'lastreply' => $uid,
+                'lastreply' => $userId,
                 'lastup' => Carbon::now(),
             ]);
         }
 
         return response()->json(['success' => true]);
+    }
+
+    /**
+     * Format the reply message.
+     *
+     * @param string $reply
+     * @param int $userId
+     * @return string
+     */
+    private function formatMessage($reply, $userId)
+    {
+        return '
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="ticket">' . htmlspecialchars($reply) . '</div>
+                </div>
+                <div class="panel-footer">
+                    <div class="label label-info">' . $userId . '</div> - ' . Carbon::now()->format('d/m/Y h:i:s a') . '
+                </div>
+            </div>';
     }
 }
