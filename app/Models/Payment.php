a498bd94fdc2dc9e38bbc2e40d92d6364ad234ad
Elitetoolslife <elitetoolsife@gmail.com>
2024-12-07 02:14:15 +0100

    Update Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -1,1 +1,24 @@
-a
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'method',
+        'amount',
+        'amountusd',
+        'address',
+        'p_data',
+        'state',
+        'date',
+    ];
+
+    // Define any necessary relationships or methods here
+}
