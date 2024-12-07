22edd9726f9e5876e2fed1302295d637e914e73a
Elitetoolslife <elitetoolsife@gmail.com>
2024-12-07 04:15:56 +0100

    Update create.blade.php

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -1,3 +1,19 @@
-<?php 
+@extends('layouts.buyer')
 
-<!--resources/views/tickets/create.blade.php->
+@section('content')
+<div class="container">
+    <h1>Create Ticket</h1>
+    <form action="{{ route('tickets.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="subject">Subject</label>
+            <input type="text" name="subject" id="subject" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="message">Message</label>
+            <textarea name="message" id="message" class="form-control" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+@endsection
